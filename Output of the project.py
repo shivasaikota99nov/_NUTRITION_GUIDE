094264d83231cacef476798bf73aa23f2e5e34c5
shivasaikota99nov <shivasaikota99@gmail.com>
2025-11-23 23:12:43 +0530

    Add files via upload

---
+++ Output of the project.py
@@ -0,0 +1,60 @@
+Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+=== RESTART: C:/Users/shiva/AppData/Local/Programs/Python/Python313/output.py ==
+------ NUTRITION GUIDE ------
+Enter your age: 24
+
+Nutrition Suggestion:
+Age Group: Adults (20-60 years)
+- Balanced diet: rice/roti + dal + sabji
+- Lean proteins (dal, chicken, fish)
+- Seasonal fruits
+- Fiber-rich vegetables
+
+=== RESTART: C:/Users/shiva/AppData/Local/Programs/Python/Python313/output.py ==
+------ NUTRITION GUIDE ------
+Enter your age: 2
+
+Nutrition Suggestion:
+Age Group: Infants (0-3 years)
+- Breast milk or formula
+- Soft mashed fruits (banana, apple)
+- Mashed vegetables (carrot, potato)
+- Iron-rich cereals
+>>> 
+=== RESTART: C:/Users/shiva/AppData/Local/Programs/Python/Python313/output.py ==
+------ NUTRITION GUIDE ------
+Enter your age: 11
+
+Nutrition Suggestion:
+Age Group: Children (4-12 years)
+- Milk, eggs, fruits
+- Vegetables and whole grains
+- Light snacks (nuts, healthy sandwiches)
+- Adequate water
+>>> 
+=== RESTART: C:/Users/shiva/AppData/Local/Programs/Python/Python313/output.py ==
+------ NUTRITION GUIDE ------
+Enter your age: 15
+
+Nutrition Suggestion:
+Age Group: Teenagers (13-19 years)
+- High-protein foods (egg, chicken, paneer)
+- Calcium-rich foods (milk, curd)
+- Fruits and salads
+- Avoid junk food
+>>> 
+=== RESTART: C:/Users/shiva/AppData/Local/Programs/Python/Python313/output.py ==
+------ NUTRITION GUIDE ------
+Enter your age: 
+=== RESTART: C:/Users/shiva/AppData/Local/Programs/Python/Python313/output.py ==
+------ NUTRITION GUIDE ------
+Enter your age: 77
+
+Nutrition Suggestion:
+Age Group: Senior Citizens (60+ years)
+- Soft, easy-to-digest food
+- Calcium and vitamin D rich foods
+- Low salt and low sugar diet
+- Hydration and soups
